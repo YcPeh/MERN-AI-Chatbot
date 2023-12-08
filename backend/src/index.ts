c9f5c23cd0e66233a0f278eb3563f71de7f4a101
YcPeh <104683280+YcPeh@users.noreply.github.com>
2023-12-08 23:39:47 +0800

    Integrate MongoDB Database Connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,12 @@
-console.log("hello world");
+import app from "./app.js";
+import { connectToDatabase } from "./db/connection.js";
+
+// connections and listeners
+const PORT = process.env.PORT || 5000;
+connectToDatabase()
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log(`Server ${PORT} Opened and Connected to Database`)
+    );
+  })
+  .catch((error) => console.log(error));
